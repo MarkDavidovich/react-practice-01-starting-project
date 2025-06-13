2e159cc4c3dee28da54cf6b044ff36f1ee6ad2a1
Mark Davidovich <mark.davidovich@gmail.com>
2025-06-13 18:05:57 +0300

    add custom component for sections

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import TabButton from "./TabButton";
 import { useState } from "react";
 import { EXAMPLES } from "../data";
+import Section from "./Section";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -10,7 +11,7 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
+    <Section title="Examples" id="examples">
       <h2>Examples</h2>
       <menu>
         <TabButton
@@ -49,6 +50,6 @@ export default function Examples() {
           </pre>
         </div>
       )}
-    </section>
+    </Section>
   );
 }
