2d9d632f205625a78570a7fbe06c2e2293b9893a
Mark Davidovich <mark.davidovich@gmail.com>
2025-06-14 12:45:16 +0300

    slightly rework tabContent logic add tabs component

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,13 +1,29 @@
-import TabButton from "./TabButton";
 import { useState } from "react";
-import { EXAMPLES } from "../data";
+import TabButton from "./TabButton";
 import Section from "./Section";
+import Tabs from "./Tabs";
+
+import { EXAMPLES } from "../data";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -39,17 +55,7 @@ export default function Examples() {
           State
         </TabButton>
       </menu>
-      {!selectedTopic ? (
-        <p>Please select a topic.</p>
-      ) : (
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic].code}</code>
-          </pre>
-        </div>
-      )}
+      {tabContent}
     </Section>
   );
 }
