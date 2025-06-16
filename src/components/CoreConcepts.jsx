884aab1e3b1fcd6521a13ea8b8e07905201fd4e1
Mark <mark.davidovich@gmail.com>
2025-06-17 00:01:20 +0300

    add tabs component functionality

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -5,7 +5,6 @@ import Section from "./Section";
 export default function CoreConcepts() {
   return (
     <Section title="Core Concepts" id="core-concepts">
-      <h2>Core Concepts</h2>
       <ul>
         {CORE_CONCEPTS.map((conceptItem) => (
           <CoreConcept key={conceptItem.title} {...conceptItem} />
